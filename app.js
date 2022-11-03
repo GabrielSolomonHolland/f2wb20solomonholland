ec4861a464e8c0bae76386991a71ad21ef1b82b4
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-02 21:08:35 -0500

    part 6 (second one)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vehicleRouter = require('./routes/vehicles');
-
+var gridbuildRouter = require('./routes/gridbuild');
 var app = express();
 
 // view engine setup
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //default
 //app.use('/', indexRouter);
-app.use('/', vehicleRouter);
+//app.use('/', vehicleRouter);
+app.use('/', gridbuildRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
